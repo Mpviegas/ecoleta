ef32b5699eca40f45a3b4b4a01df66f4a4da55f0
MpViegas <mpvmmv@gmail.com>
2020-06-05 19:08:05 -0300

    src

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const server = express()
 
+// Pegar o banco de dados
+const db = require("./database/db")
+
 // Configurar pasta pública
 server.use(express.static("public"))
+
+// Habilitar o uso do req:body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // Udando uma template engine
 const nunjucks = require("nunjucks")
@@ -20,11 +26,76 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+    // req.query: Query Strings da nossa URL
+
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+    // req.body: O corpo do nosso formulário
+    console.log(req.body)
+
+    // Inserir dados no banco de dados
+    const query = `
+        INSERT INTO places (
+            image,
+            name, 
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    const search = req.query.search
+
+    if (search == "") {
+        return res.render("search-results.html", { total: 0 })
+    }
+
+    // Pegar os dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        // O lenght é uma propriedade que conta quantos elementos há dentro de um array
+        const total = rows.length
+
+        // Mostrar a página html com os dados do banco de dados 
+        return res.render("search-results.html", { places: rows, total: total })
+    })
+
 })
 
 // Ligar o servidor
